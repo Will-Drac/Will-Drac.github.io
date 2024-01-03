7dd1a6d517ce175652671c54c0f08e05e196996b
WillDrac <willdrac@yahoo.com>
2024-01-02 21:18:14 -0500

    genuary second

--- setup.js
+++ setup.js
@@ -1,5 +1,9 @@
 const ProjectList = [
     {
+        url: "Genuary 2024/2 - No palettes/index.html",
+        folder: "Genuary 2024"
+    },
+    {
         url: "Genuary 2024/1 - Particles, lots of them/index.html",
         folder: "Genuary 2024"
     },
